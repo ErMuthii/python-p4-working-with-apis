007bec6b18c85d2995600b366f5f51c34c75e978
ErMuthii <er.muthii@gmail.com>
2024-03-18 15:56:59 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,25 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+         programs_list.append(program["building_fee_paid"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+    return programs_list
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+  
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
